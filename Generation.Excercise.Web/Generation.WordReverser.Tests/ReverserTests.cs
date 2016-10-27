313947c96bd50be8c0aa4af6a3a71e0ddd4a18fa
Jens Pettersson <jens.pettersson@live.se>
2016-10-27 18:25:26 +0200

    Added test project

---
+++ Generation.Excercise.Web/Generation.WordReverser.Tests/ReverserTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace Generation.WordReverser.Tests
+{
+    [TestFixture]
+    public class ReverserTests
+    {
+        private Reverser _reverser;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _reverser = new Reverser();
+        }
+
+        [Test]
+        public void Should_reverse_one_word()
+        {
+            string result = _reverser.Reverse("hello");
+
+            Assert.AreEqual("olleh", result);
+        }
+
+        [Test]
+        public void Should_reverse_words_in_sentence_and_keep_them_in_place()
+        {
+            string result = _reverser.Reverse("hello world");
+
+            Assert.AreEqual("olleh dlrow", result);
+        }
+
+        [Test]
+        public void Should_keep_uppercase_positions()
+        {
+            string result = _reverser.Reverse("For the Win");
+
+            Assert.AreEqual("Rof eht Niw", result);
+
+        }
+    }
+}
---
+++ Generation.Excercise.Web/Generation.WordReverser.Tests/WordTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace Generation.WordReverser.Tests
+{
+    [TestFixture]
+    public class WordTests
+    {
+        [Test]
+        public void Should_reverse_given_word()
+        {
+            var reversed = new Word("hello").Reversed;
+            Assert.AreEqual("olleh", reversed);
+        }
+
+        [Test]
+        public void Should_keep_uppercase_positions()
+        {
+            var reversed = new Word("Hello").Reversed;
+            Assert.AreEqual("Olleh", reversed);
+        }
+
+        [Test]
+        public void All_upper_case_should_return_all_uppercase()
+        {
+            var reversed = new Word("HELLO").Reversed;
+            Assert.AreEqual("OLLEH", reversed);
+        }
+    }
+}
