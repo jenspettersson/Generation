165a00391987f0b4c810e353d00f374eae212ec6
Jens Pettersson <jens.pettersson@live.se>
2016-11-03 14:15:48 +0100

    Added Painter excercise

---
+++ Generation.Excercise.Web/Generation.Excercise.Web/Controllers/PainterController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Generation.Excercise.Web.Controllers
+{
+    public class PainterController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
